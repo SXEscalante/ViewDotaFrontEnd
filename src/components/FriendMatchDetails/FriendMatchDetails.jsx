b06b8b33eb3a360829fc16805c5731e0d0c57243
Sebatian <Sxe1exs@gmail.com>
2024-01-09 13:05:04 -0700

    Basic components added to match details page

---
+++ src/components/FriendMatchDetails/FriendMatchDetails.jsx
@@ -0,0 +1,37 @@
+const FriendMatchDetails = ({}) => {
+    return ( 
+        <div className="match-details">
+            <div>
+                <div>
+                    <p>Duration</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>Damage</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>Heal</p>
+                    <p></p>
+                </div>
+                <div>
+                    <p>Net worth</p>
+                    <p></p>
+                </div>
+            </div>
+            <div className="sidebar">
+                <div className="hero-info">
+                    <img src="" alt="" />
+                    <p>Kills</p>
+                    <p>Deaths</p>
+                    <p>Assists</p>
+                </div>
+                <div className="items">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default FriendMatchDetails;
--- src/pages/MatchDetailsPage/MatchDetailsPage.jsx
+++ src/pages/MatchDetailsPage/MatchDetailsPage.jsx
@@ -1,6 +1,46 @@
+import FriendMatchDetails from "../../components/FriendMatchDetails/FriendMatchDetails";
+import "./MatchDetailsPage.css"
+
 const MatchDetailsPage = ({}) => {
     return ( 
-        <div></div>
+        <div className="match-page">
+            <div className="match-details">
+                <div>
+                    <div>
+                        <p>Duration</p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p>Damage</p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p>Heal</p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p>Net worth</p>
+                        <p></p>
+                    </div>
+                    <FriendMatchDetails/>  
+                </div>
+                <div className="sidebar">
+                    <div className="hero-info">
+                        <img src="" alt="" />
+                        <p>Kills</p>
+                        <p>Deaths</p>
+                        <p>Assists</p>
+                    </div>
+                    <div className="items">
+
+                    </div>
+                </div>
+            </div>
+            <div className="match-comments">
+                <h2>Comments</h2>
+                
+            </div>
+        </div>
     );
 }
  
